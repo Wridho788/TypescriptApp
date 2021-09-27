9cdd7dc791dc15c78e4b49c844df1130d4c0eec2
Github <wridho246@gmail.com>
2021-09-27 16:14:36 +0700

    add style component and style system

---
+++ screens/wellcome/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Wellcome = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+};
+
+export default Wellcome;
+
+const styles = StyleSheet.create({});
